4f04dbf63d4dee198782474651779608626bf5b0
Federico Brigante <bfred-it@users.noreply.github.com>
2016-04-06 17:27:59 +0700

    Initial version

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import onChange from './lib/manual-on-change';
+let handlers = [];
+let timer;
+
+function getVisibility() {
+	return document.hidden;
+}
+const observer = onChange(
+	getVisibility,
+	() => handlers.forEach(fn => setTimeout(fn, 0))
+);
+
+function maybeStartLoop() {
+	if (!timer) {
+		timer = setInterval(observer.verify, 333);
+	}
+}
+function maybeEndLoop() {
+	if (!handlers.length) {
+		timer = clearInterval(timer);
+	}
+}
+function onVisibilityChange(handler) {
+	// Use onChange to make sure that the handler is
+	// only called once by either the browser or the ponyfill
+	const debouncer = onChange(getVisibility, handler);
+
+	document.addEventListener('visibilitychange', debouncer.verify);
+	handlers.push(debouncer.verify);
+
+	maybeStartLoop();
+	const remove = function () {
+		handlers = handlers.filter(fn => fn !== debouncer.verify);
+		document.removeEventListener('visibilitychange', debouncer.verify);
+		maybeEndLoop();
+	};
+
+	// Store the remove function on the handler
+	// so it can be called by removeAll()
+	debouncer.verify.remove = remove;
+	return {remove};
+}
+onVisibilityChange.removeAll = function () {
+	handlers.forEach(fn => {
+		fn.remove();
+	});
+	handlers.length = 0;
+	maybeEndLoop();
+};
+export default onVisibilityChange;
---
+++ readme.md
@@ -0,0 +1,52 @@
+# visibility-change-ponyfill
+
+> A more reliable visibilitychange event that works in more mobile WebViews
+
+[![gzipped size](https://badges.herokuapp.com/size/github/bfred-it/visibility-change-ponyfill/master/dist/visibility-change-ponyfill.browser.js?gzip=true&label=gzipped%20size)](#readme) [![Travis build status](https://api.travis-ci.org/bfred-it/visibility-change-ponyfill.svg?branch=master)](https://travis-ci.org/bfred-it/visibility-change-ponyfill) 
+
+## Install
+
+```sh
+npm install --save visibility-change-ponyfill
+```
+```js
+import onVisibilityChange from 'visibility-change-ponyfill';
+```
+
+If you don't use node/babel, include this:
+
+```html
+<script src="dist/visibility-change-ponyfill.browser.js"></script>
+```
+
+## Usage
+
+To add a listener:
+
+```js
+onVisibilityChange(function () {
+	if(document.hidden) {
+		// this page is now hidden
+	} else {
+		// this page is now visible
+	}
+});
+```
+
+To remove a listener:
+
+```js
+var listener = onVisibilityChange(function () { /* */ });
+
+listener.remove();
+```
+
+To remove all listeners:
+
+```js
+onVisibilityChange.removeAll();
+```
+
+## License
+
+MIT © [Federico Brigante](http://twitter.com/bfred_it)
