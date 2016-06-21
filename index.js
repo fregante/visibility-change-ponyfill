3257d037e2c38ec2e98d98224eae2d08932de65a
Federico Brigante <bfred-it@users.noreply.github.com>
2016-06-21 19:04:39 +0700

    Simplified code, changed API
    # Only one ‘visibilitychange’ listener is attached now, calling all the
    handlers.
    
    # There’s new way to remove an event listener:
    
    Old way:
    - onVisibilityChange(fn).remove()
    
    New way:
    - onVisibilityChange.remove(fn)

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
 'use strict';
 
 import onChange from './lib/manual-on-change';
+
 let handlers = [];
-let timer;
+let isBeingTracked;
 
-function getVisibility() {
-	return document.hidden;
-}
-
-const observer = onChange(
-	getVisibility,
+const verifyVisibilityChange = onChange(
+	() => document.hidden,
 	() => {
 		handlers.forEach(setTimeout); // with setTimeout errors won't stop the loop
 	}
 );
 
-function maybeStartLoop() {
-	if (!timer) {
-		timer = setInterval(observer.verify, 333);
+function maybeStartTracking() {
+	if (!isBeingTracked) {
+		isBeingTracked = setInterval(verifyVisibilityChange, 333);
+		document.addEventListener('visibilitychange', verifyVisibilityChange);
 	}
 }
-function maybeEndLoop() {
+
+function maybeStopTracking() {
 	if (!handlers.length) {
-		timer = clearInterval(timer);
+		isBeingTracked = clearInterval(isBeingTracked);
+		document.removeEventListener('visibilitychange', verifyVisibilityChange);
 	}
 }
+
 function onVisibilityChange(handler) {
-	// Use onChange to make sure that the handler is
-	// only called once by either the browser or the ponyfill
-	const debouncer = onChange(getVisibility, handler);
-
-	document.addEventListener('visibilitychange', debouncer.verify);
-	handlers.push(debouncer.verify);
-
-	maybeStartLoop();
-	const remove = function (onlyRemoveEventListener) {
-		document.removeEventListener('visibilitychange', debouncer.verify);
-		if (!onlyRemoveEventListener) {
-			handlers = handlers.filter(fn => fn !== debouncer.verify);
-			maybeEndLoop();
-		}
-	};
-
-	// Store the remove function on the handler
-	// so it can be called by removeAll()
-	debouncer.verify.remove = remove;
-	return {remove};
+	handlers.push(handler);
+	maybeStartTracking();
 }
-onVisibilityChange.removeAll = function () {
-	handlers.forEach(fn => {
-		fn.remove(true);
-	});
-	handlers.length = 0;
-	maybeEndLoop();
+
+onVisibilityChange.remove = handler => {
+	handlers = handlers.filter(fn => fn !== handler);
+	maybeStopTracking();
 };
+
+onVisibilityChange.removeAll = () => {
+	handlers.length = 0;
+	maybeStopTracking();
+};
+
 export default onVisibilityChange;
